112950101d6941bf447663abfbf9c6306605e99b
jsaezbu <jsaez26@xtec.cat>
2025-07-10 22:52:52 +0200

    Add files via upload

---
+++ target/wasm32v1-none/release/deps/certifica_2_wasm.d
@@ -0,0 +1,5 @@
+/home/pwd_is_123/certifica-2/target/wasm32v1-none/release/deps/certifica_2_wasm.wasm: src/lib.rs
+
+/home/pwd_is_123/certifica-2/target/wasm32v1-none/release/deps/certifica_2_wasm.d: src/lib.rs
+
+src/lib.rs:
